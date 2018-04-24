d9551559a9faab9e0521d5224acd98d4b91ee028
Sean <seanfindlay3@gmail.com>
2018-04-24 21:02:32 +0200

    Add Immutability

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -1,7 +1,9 @@
 package com.chess.engine.board;
 
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public abstract class Tile {
@@ -9,7 +11,21 @@ public abstract class Tile {
     protected final int tileCoordinate;
     private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
 
-    Tile(int tileCoordinate){
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles(){
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for(int i = 0; i < 64; i++){
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
+    public static Tile createTile(final int tileCoordinate, final Piece piece){
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+    }
+
+    private Tile(int tileCoordinate){
         this.tileCoordinate = tileCoordinate;
 
     }
