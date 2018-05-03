e2db7e6a7ae2160442e173cef80a99ffacbb98d1
Sean Findlay <seanfindlay3@gmail.com>
2018-05-03 20:33:40 +0200

    Add to Pawn class

--- src/com/chess/engine/pieces/Pawn.java
+++ src/com/chess/engine/pieces/Pawn.java
@@ -4,6 +4,7 @@ import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
+import com.chess.engine.board.Move.MajorMove;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -13,7 +14,7 @@ public class Pawn extends Piece{
 
     private final static int[] CANDIDATE_MOVE_COORDINATE = {8};
 
-    Pawn(int piecePosition, Alliance pieceAlliance) {
+    Pawn(final int piecePosition, final Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
     }
 
@@ -31,8 +32,18 @@ public class Pawn extends Piece{
             }
 
             if(currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
-                //TODO more to add here!!!
-                legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+                //TODO more to add here(deal with promotions)!!!
+                legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+            }else if(currentCandidateOffset == 16 && this.isFirstMove() &&
+                    (BoardUtils.SECOND_ROW[this.piecePosition] && this.getPieceAlliance().isBlack()) ||
+                    (BoardUtils.SEVENTH_ROW[this.piecePosition] && this.getPieceAlliance().isWhite())) {
+                final int behindCandidateDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * 8);
+                if(!board.getTile(behindCandidateDestinationCoordinate).isTileOccupied() &&
+                        !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                    legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                }
+
+
             }
 
         }
