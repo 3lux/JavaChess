18d1dc9bb71e80d6e0f69cf75270dd5a160256aa
Sean Findlay <seanfindlay3@gmail.com>
2018-05-05 21:00:38 +0200

    Add Board(part II)

--- src/com/chess/engine/board/Board.java
+++ src/com/chess/engine/board/Board.java
@@ -1,22 +1,42 @@
 package com.chess.engine.board;
 
 import com.chess.engine.Alliance;
-import com.chess.engine.pieces.Piece;
+import com.chess.engine.pieces.*;
 import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 public class Board{
 
     private final List<Tile> gameBoard;
+    private final Collection<Piece> whitePieces;
+    private final Collection<Piece> blackPieces;
 
     private Board(Builder builder) {
         this.gameBoard = createGameBoard(builder);
+        this.whitePieces = calculateActivePieces(this.gameBoard, Alliance.WHITE);
+        this.blackPieces = calculateActivePieces(this.gameBoard, Alliance.BLACK);
+    }
+
+    private Collection<Piece> calculateActivePieces(final List<Tile> gameBoard,
+                                                    Alliance alliance) {
+        final List<Piece> activePieces = new ArrayList<>();
+        for(final Tile tile : gameBoard) {
+            if(tile.isTileOccupied()) {
+                final Piece piece = tile.getPiece();
+                if(piece.getPieceAlliance() == alliance) {
+                    activePieces.add(piece);
+                }
+            }
+        }
+        return ImmutableList.copyOf(activePieces);
     }
 
     public Tile getTile(final int tileCoordinate){
-        return null;
+        return gameBoard.get(tileCoordinate);
     }
 
     private static List<Tile> createGameBoard(final Builder builder) {
@@ -28,7 +48,45 @@ public class Board{
     }
 
     public static Board createStandardBoard() {
+        final Builder builder = new Builder();
+        // Black Layout
+        builder.setPiece(new Rook(0, Alliance.BLACK));
+        builder.setPiece(new Knight(1, Alliance.BLACK));
+        builder.setPiece(new Bishop(2, Alliance.BLACK));
+        builder.setPiece(new Queen(3, Alliance.BLACK));
+        builder.setPiece(new King(4, Alliance.BLACK));
+        builder.setPiece(new Bishop(5, Alliance.BLACK));
+        builder.setPiece(new Knight(6, Alliance.BLACK));
+        builder.setPiece(new Rook(7, Alliance.BLACK));
+        builder.setPiece(new Pawn(8, Alliance.BLACK));
+        builder.setPiece(new Pawn(9, Alliance.BLACK));
+        builder.setPiece(new Pawn(10, Alliance.BLACK));
+        builder.setPiece(new Pawn(11, Alliance.BLACK));
+        builder.setPiece(new Pawn(12, Alliance.BLACK));
+        builder.setPiece(new Pawn(13, Alliance.BLACK));
+        builder.setPiece(new Pawn(14, Alliance.BLACK));
+        builder.setPiece(new Pawn(15, Alliance.BLACK));
+        // White Layout
+        builder.setPiece(new Rook(48, Alliance.WHITE));
+        builder.setPiece(new Knight(49, Alliance.WHITE));
+        builder.setPiece(new Bishop(50, Alliance.WHITE));
+        builder.setPiece(new Queen(51, Alliance.WHITE));
+        builder.setPiece(new King(52, Alliance.WHITE));
+        builder.setPiece(new Bishop(53, Alliance.WHITE));
+        builder.setPiece(new Knight(54, Alliance.WHITE));
+        builder.setPiece(new Rook(55, Alliance.WHITE));
+        builder.setPiece(new Pawn(56, Alliance.WHITE));
+        builder.setPiece(new Pawn(57, Alliance.WHITE));
+        builder.setPiece(new Pawn(58, Alliance.WHITE));
+        builder.setPiece(new Pawn(59, Alliance.WHITE));
+        builder.setPiece(new Pawn(60, Alliance.WHITE));
+        builder.setPiece(new Pawn(61, Alliance.WHITE));
+        builder.setPiece(new Pawn(62, Alliance.WHITE));
+        builder.setPiece(new Pawn(63, Alliance.WHITE));
+        // White to move
+        builder.setMoveMaker(Alliance.WHITE);
 
+        return builder.build();
     }
 
     public static class Builder {
